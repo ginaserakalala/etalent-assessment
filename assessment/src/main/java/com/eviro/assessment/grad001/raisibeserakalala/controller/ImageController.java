cb9c7ab40b242e4d564e89bf4b35869906d00afe
ginaserakalala <ginaserakala@gmail.com>
2022-11-04 01:05:56 +0200

    csv converter changes

--- assessment/src/main/java/com/eviro/assessment/grad001/raisibeserakalala/controller/ImageController.java
+++ assessment/src/main/java/com/eviro/assessment/grad001/raisibeserakalala/controller/ImageController.java
@@ -1,16 +1,35 @@
 package com.eviro.assessment.grad001.raisibeserakalala.controller;
 
+import com.eviro.assessment.grad001.raisibeserakalala.service.FileParser;
+import com.eviro.assessment.grad001.raisibeserakalala.service.FileParserImpl;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+
 
 @RestController
 @RequestMapping("/v1/api/image")
 
 public class ImageController {
+    FileParserImpl fpi = new FileParserImpl();
 
-    @GetMapping(value = "/{name}/{surname}/{\\w\\.\\w}")
-    public FileSystemResource gethttpImageLink(@PathVariable String name, @PathVariable String surname) {
-        String path="/home/user/Desktop/";
-        return new FileSystemResource(path + name + surname);
+    public ImageController(){
+        File csvfile = new File("/Users/ginaserakalala/Downloads/etalent_assessment/1664806858150-GraduateDev_AssessmentCsv_v2.csv");
+        fpi.parseCSV(csvfile);
+    }
+
+
+    @GetMapping(value = "/{name}/{surname}/{filename:[a-zA-Z]+.[a-zA-Z]+}")
+    //tMapping("/spring-web/{fileName:[a-z-]+}-{version:\d\.\d\.\d}{extension:\.[a-z]+}")
+    public FileSystemResource gethttpImageLink(@PathVariable String name, @PathVariable String surname, @PathVariable String filename) {
+        String[] data = FileParserImpl.rows.get(name.toLowerCase() + " " + surname.toLowerCase());
+        File convertedfile = fpi.convertCSVDataToImage(data[1]);
+        String ext = data[0].split("/")[1];
+        File target = new File("./" + filename + "." + ext);
+        return new FileSystemResource("/Users/ginaserakalala/Downloads/etalent_assessment/1664806858150-GraduateDev_AssessmentCsv_v2.csv");
+
+
+
     }
 }
--- assessment/src/main/java/com/eviro/assessment/grad001/raisibeserakalala/service/FileParserImpl.java
+++ assessment/src/main/java/com/eviro/assessment/grad001/raisibeserakalala/service/FileParserImpl.java
@@ -8,17 +8,21 @@ import com.opencsv.CSVReaderBuilder;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URI;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
 
 public class FileParserImpl implements FileParser {
+    public static HashMap <String,String[]> rows = new HashMap<>();
     @Override
     public void parseCSV(File csvFile) {
+
         try {
             FileReader filereader = new FileReader(csvFile);
 
 
-            CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+            CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
 
 
             CSVReader csvReader = new CSVReaderBuilder(filereader)
@@ -27,29 +31,44 @@ public class FileParserImpl implements FileParser {
 
             List<String[]> allData = csvReader.readAll();
 
-            for (String[] row : allData) {
-                for (String cell : row) {
-                    System.out.print(cell + "\t");
-                }
+            for (int i= 0; i < allData.size();i++) {
+                if(i == 0)
+                    continue;
+
+//                for (String cell : row) {
+//                    System.out.print(cell + "\t");
+//                }
+
+                String[] data = allData.get(i);
+                String fullname = data[0].toLowerCase() + " "  + data[1].toLowerCase() ;
+                data = Arrays.copyOfRange(data, 2, 4);
+                rows.put(fullname,data);
+
                 System.out.println();
             }
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+
+
     }
 
     @Override
     public File convertCSVDataToImage(String base64ImageData) {
         byte[] decodedBytes = Base64.getDecoder().decode(base64ImageData);
         String decodedString = new String(decodedBytes);
-        File file = new File("./test.jpeg");
+        File file = new File("./test");
 
         return file;
     }
 
     @Override
     public URI createImageLink(File fileImage) {
-        return null;
+
+        URI uri = fileImage.toURI();
+        System.out.println(uri);
+
+        return uri;
     }
 }
