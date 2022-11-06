ac9dd11cfa9f09da304a4dec428c459d638c957e
ginaserakalala <ginaserakala@gmail.com>
2022-11-06 23:07:53 +0200

    file updates

--- assessment ofc/src/main/java/com/eviro/assessment/grad001/raisibeserakalala/controller/ImageController.java
+++ assessment ofc/src/main/java/com/eviro/assessment/grad001/raisibeserakalala/controller/ImageController.java
@@ -32,7 +32,8 @@ public class ImageController {
         String ext = data[0].split("/")[1];
         File target = new File("./" + filename + "." + ext);
 
-        return new FileSystemResource("");
+
+        return new FileSystemResource(target);
 
 
     }
